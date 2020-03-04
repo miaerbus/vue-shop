e0df637b48c41aef8d4a892f4351e664958bf1a1
Mia Erbus <miaerbus@Mias-MacBook-Pro.local>
2020-03-04 09:48:52 +0100

    Navbar again

---
+++ src/views/Admin.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="admin">
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis optio, earum
+    voluptatem eos debitis placeat quae harum, dolores temporibus ad facilis
+    omnis, ex fugiat quas accusantium repudiandae minus maiores rem.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Admin'
+};
+</script>
